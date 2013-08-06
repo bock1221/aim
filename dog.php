acb8fe3e247ef644852ab7488fa398da2cb5f9e3
bock1221 <tzvibock@gmail.com>
2013-08-06 17:37:42 -0400

    first commit

---
+++ dog.php
@@ -0,0 +1,36 @@
+<? 
+
+  $record = array('first_name' => 'eliezer', 'last_name'=> 'kops');
+
+
+  
+  $records[] = $record;
+  $record['first_name']='tzvi';
+  $record['last_name'] = 'bock';
+  
+
+  $records[] = $record;
+  $record['first_name']='chaim';
+  $record['last_name']='farkas';
+  $records[] = $record; 
+
+  print_r($records);
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
